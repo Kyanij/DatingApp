361224dff99020340411a7ba430da8a2cb0b2ee8
Kyanij Maharjan <mkyanij1@gmail.com>
2021-01-02 21:47:20 +0545

    added persistent login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +12,25 @@ export class AppComponent implements OnInit {
   title = 'DatingApp';
   users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) { }
 
   ngOnInit(): any {
     this.getUsers();
+    this.setCurrentUser();
+
+  }
+
+  setCurrentUser() {
+    const temp: any = localStorage.getItem('user');
+    const user: User = JSON.parse(temp);
+    this.accountService.setCurrentUser(user);
 
   }
 
   getUsers(): any {
     this.http.get('https://localhost:5001/api/users').subscribe(response => {
       this.users = response;
+      console.log(response);
     }, error => console.log(error));
   }
 
