528ca251809347919ca1143e67c8655ba98a3954
Kyanij Maharjan <mkyanij1@gmail.com>
2021-01-05 17:50:53 +0545

    added toast package

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,7 +10,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
   model: any = {};
 
   ngOnInit(): void {
@@ -16,14 +18,17 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
+      this.router.navigateByUrl('/messages');
       console.log(response);
     }, error => {
+      this.toastr.error(error.error);
       console.log(error);
     })
   }
 
   logout() {
-    this.accountService.logout()
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 
